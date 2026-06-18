a51f49cf9ad43ecc5c24143351ab8274cb50f533
arsal3646 <arsal3646@gmail.com>
2026-06-18 20:27:39 +0400

    I learnt three different types of operating systems

--- Udemy_Abdul_Bari_Course_2026/001_Instructor_Note.java
+++ Udemy_Abdul_Bari_Course_2026/001_Instructor_Note.java
@@ -4,3 +4,26 @@ public class HelloWorld {
     }
 }
 
+/*  I want to know different famous types of operating systems:
+
+    1. MacOS: Developed by Apple Inc., 
+            -- It is known for its sleek design and user-friendly interface. 
+            -- It is widely used in creative industries.
+
+    2. Windows: 
+            -- Developed by Microsoft
+            -- It is the most widely used operating system for personal computers (PC). 
+            -- It offers a wide range of software compatibility and is popular in business environments.
+
+    3. Linux: 
+            -- An open-source operating system that is highly customizable and secure. 
+            -- It is popular among developers and is used in servers, supercomputers, and embedded systems.
+
+            -- open-source means that the source code of the operating system is freely available for anyone 
+                i. to view
+                ii. modify
+                iii. distribute. 
+                        
+*/
+
+
