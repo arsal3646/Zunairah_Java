fd9ad639bef29182856786ab032bcef7608c43e8
arsal3646 <arsal3646@gmail.com>
2026-06-19 13:25:55 +0400

    I learnt what JDK is

---
+++ Udemy_Abdul_Bari_Course_2026/002_Download_JDK.java
@@ -0,0 +1,10 @@
+/* >JDK stands for Java Development Kit. It is a software development environment used for developing Java applications and applets. It includes the Java Runtime Environment (JRE), an interpreter/loader (Java), a compiler (javac), an archiver (jar), a documentation generator (Javadoc), and other tools needed for Java development.
+ To download JDK, you can follow these steps:
+      1. Go to the official Oracle website: https://www.oracle.com/java/technologies/javase-downloads.html
+      2. Click on the "JDK Download" button.  
+      3. You will be redirected to the download page. Here, you can choose the version of JDK you want to download (e.g., JDK 17, JDK 11, etc.).
+      4. Select the appropriate version for your operating system (Windows, macOS, or Linux).
+      5. Click on the download link for the selected version and operating system.
+
+   >Javac is a compiler that is part of the the JDK. 
+*/
